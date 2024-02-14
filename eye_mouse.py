62882cf909c1892a7259f499aa6eb4094d6f83bf
Ali0049 <alij75026@gmail.com>
2024-02-14 18:38:05 +0500

    Initial commit

---
+++ eye_mouse.py
@@ -0,0 +1,35 @@
+import cv2
+import mediapipe as mp
+import pyautogui
+cam = cv2.VideoCapture(0)
+face_mesh = mp.solutions.face_mesh.FaceMesh(refine_landmarks=True)
+screen_w , screen_h = pyautogui.size()
+while True:
+    _ , frame = cam.read()
+    frame = cv2.flip(frame, 1)
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    output = face_mesh.process(rgb_frame)
+    landmarks_points = output.multi_face_landmarks
+    frame_h, frame_w , _ = frame.shape
+    if landmarks_points:
+        landmarks = landmarks_points[0].landmark
+        for id, landmark in enumerate(landmarks[474:478]):
+            x = int(landmark.x * frame_w)
+            y = int(landmark.y * frame_h)
+            cv2.circle(frame, (x, y), 3 ,(0, 255,0))
+            if id == 1:
+                screen_x = screen_w / frame_w * x
+                screen_y = screen_h /frame_h * y
+                pyautogui.moveTo(screen_x, screen_y)
+            left = [landmarks[145], landmarks[159]]
+            for landmark in left:
+                x = int(landmark.x * frame_w)
+                y = int(landmark.y * frame_h)
+                cv2.circle(frame, (x, y), 3 ,(0, 255,255))
+            print(left[0].y - left[1].x)
+            if (left[0].y - left[1].x) <0.01:
+                print('click')    
+
+    cv2.imshow('eye_mouse',frame)
+    cv2.waitKey(1)
+
